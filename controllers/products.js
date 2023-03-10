bedfe0cb2d839de16360b6bb9bb40453f4c3756e
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-10 12:40:28 +0200

    numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, price } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,12 +21,24 @@ const getAllProducts = async (req, res) => {
   }
 
   let result = Product.find(queryObject);
+  //sort method
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  //pagination skip
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
