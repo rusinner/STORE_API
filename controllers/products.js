0eec69f165ba97238e7e00f31edcb46fdb417156
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-10 12:12:57 +0200

    sorting methods

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,12 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "vase table",
-  });
+  const products = await Product.find({}).sort("name");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort, price } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -18,7 +16,18 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  const products = await Product.find(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 module.exports = {
